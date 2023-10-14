61f2964d311a3c273ed8476c1ea0e07b5dbc5c44
anjanayraina <anjanayraina326@gmail.com>
2023-10-14 21:53:38 +0530

    structs added

--- contracts/Manager.sol
+++ contracts/Manager.sol
@@ -1,17 +1,30 @@
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity 0.8.5;
-contract Manager {
+pragma solidity ^0.8.20;
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+contract Manager  is Ownable{
+
+    constructor() Ownable(msg.sender){
+        
+    }
     mapping(address=>mapping(address=>bool)) approvedAddresses;
 
     struct Doctor {
         string name;
         address walletAddress;
-        
+
+    }
+
+    struct Diesease {
+        string name;
+        string []symptoms;
+
 
     }
     struct Patient {
         string name;
         address walletAddress;
+
+
 
     }
 }
