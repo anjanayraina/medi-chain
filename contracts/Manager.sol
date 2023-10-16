e5e5183a501e74b75728637d5bb63bd54351679b
anjanayraina <anjanayraina326@gmail.com>
2023-10-16 22:33:32 +0530

    Functions Created

--- contracts/Manager.sol
+++ contracts/Manager.sol
@@ -6,26 +6,63 @@ contract Manager  is Ownable{
     constructor() Ownable(msg.sender){
         
     }
-    mapping(address=>mapping(address=>bool)) approvedAddresses;
-
+    mapping(address => Doctor) approvedDoctors;
+    mapping (address => Patient) allPatients;
     struct Doctor {
         string name;
         address walletAddress;
+        Patient []allPatients;
 
     }
 
     struct Diesease {
         string name;
         string []symptoms;
-        string [] 
-
+        bool active;
 
     }
     struct Patient {
         string name;
+        uint8 weight;
+        string patientAddress;
+        uint8 bloodPressureUp;
+        uint8 bloodPressureDown;
+        uint256 lastUpdated;
+        uint24 bloodGlucose;
         address walletAddress;
-
-
+        Diesease [] allDieseases;
 
     }
+
+    modifier patientPresent {
+        require(allPatients[msg.sender].weight != 0  , "Patient Not Present");
+        _; 
+    }
+
+    function _isDoctor(address walletAddress) internal view returns (bool){
+        return approvedDoctors[walletAddress].walletAddress != address(0);
+    }
+
+    function addDoctor(string calldata _name , address _walletAddress) external onlyOwner {
+        require(!_isDoctor(_walletAddress), "Doctor Already Present");
+        Doctor storage doctor = approvedDoctors[_walletAddress];
+        doctor.name = _name;
+        doctor.walletAddress = _walletAddress;
+
+    }
+
+    function addPatient(string calldata _name , uint8 _weight , string calldata _patientAddress ) external {        
+        require(_isDoctor(msg.sender) , "Not an Apporved Doctor");
+        require(allPatients[msg.sender].weight == 0  , "Patient Already Present");
+        Patient storage patient = allPatients[msg.sender];
+        patient.name = _name;
+        patient.weight = _weight;
+        patient.patientAddress = _patientAddress;
+    }
+
+
+
+
+
+
 }
